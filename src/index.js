bbdd626ef5671a0487cd799a249acb917e808c7c
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-10-09 15:47:18 -0300

    new modifies

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Home } from './templates/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Home newProp='new prop'/>
   </React.StrictMode>
 );
 
