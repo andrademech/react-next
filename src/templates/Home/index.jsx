cfe040c1d699fd6dc432498cd2495d83e2550cba
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-10-11 09:15:46 -0300

    making tests with jest

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -88,3 +88,35 @@ export const Home = () => {
   );
 
 }
+
+// import "./styles.css"
+
+// import { Component } from "react"
+
+// export class Home extends Component {
+//   state = {
+//     counter: 0
+//   }
+
+//   handleClick = () => {
+
+//     this.setState(
+//       (prevState, prevProps) => {
+//         return { counter: prevState.counter + 1 }
+//       },
+//       () => {
+//         console.log(this.state.counter);
+//       }
+//     )
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <h1>{this.state.counter}</h1>
+//         <button onClick={this.handleClick}>Increment</button>
+//       </div>
+//     )
+//   }
+
+// }
